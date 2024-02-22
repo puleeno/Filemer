4851630b24f116cf09266a563036c0aebe054524
Puleeno <puleeno@gmail.com>
2024-02-22 09:23:44 +0700

    Close console

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// https://github.com/slint-ui/slint/issues/1190
+#![windows_subsystem = "windows"]
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
