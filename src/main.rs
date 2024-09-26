de505fe728badc02f449cebecdbea1a391f5fb14
Puleeno <puleeno@gmail.com>
2024-09-26 20:35:26 +0700

    Reset project and start coding from scratch

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,3 @@
-// https://github.com/slint-ui/slint/issues/1190
-#![windows_subsystem = "windows"]
-
-slint::include_modules!();
-
-fn main() -> Result<(), slint::PlatformError> {
-    let ui = SimpleWindows::new()?;
-
-    ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
-        }
-    });
-
-    ui.run()
+fn main() {
+    println!("Hello, world!");
 }
